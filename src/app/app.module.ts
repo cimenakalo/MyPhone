b7b9465688f5e6af17fe5138c4aa75a60c8c8a6a
cimenakalo <cimenakalo@gmail.com>
2024-06-23 21:55:54 +0300

    master

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,16 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ProductComponent } from './Components/product/product.component';
+import { SuggestedProductsComponent } from './Components/suggested-products/suggested-products.component';
+import { HomeComponent } from './Components/home/home.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ProductComponent,
+    SuggestedProductsComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule,
